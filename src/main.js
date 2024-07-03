5d5b5c1ef1d583967d3c6645886ce3399eec2850
Yuri <wall.mart.boss@proton.me>
2024-07-04 00:29:14 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -5,29 +5,40 @@ const url = "http://192.168.1.117:9000/";
 const status_EP = "get_status";
 const mode_EP = "get_mode";
 const warnings_EP = "get_warnings"; 
+const percentage_EP = "get_calculated_battery_charge"
 
 const modeUrl = `${url}${mode_EP}`;
 const statusUrl = `${url}${status_EP}`;
 const warningsUrl = `${url}${warnings_EP}`;
+const percentageUrl = `${url}${percentage_EP}`; 
 let view_mode ="";
-let view_status="";
+let view_status = "";
+let view_percentage;
 
 mode();
 statusEP();
+percentage();
 // console.log(view_mode);
 // console.log(view_status);
 // date_page.innerHTML = view_mode + view_status;
+date_page.innerHTML = view_mode + view_status + view_percentage;
 
 const setUserTimerId = setInterval(() => {
   mode();
   statusEP();
-//   console.log(view_mode);
-// console.log(view_status);
-  // date_page.innerHTML = view_mode + view_status
-}, 10000);
+    percentage();
+  date_page.innerHTML = view_mode + view_status + view_percentage
+}, 60000);
 
-  function mode() {
-    fetch(modeUrl)
+// const setUserTimerIdBat = setInterval(() => {
+//   percentage();
+// //   console.log(view_mode);
+// // console.log(view_status);
+// date_page.innerHTML = view_mode + view_status + view_percentage
+// }, 60000);
+
+  async function mode() {
+    await fetch(modeUrl)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -43,8 +54,8 @@ const setUserTimerId = setInterval(() => {
       });
   };
 
-  function statusEP() {
-    fetch(statusUrl)
+  async function statusEP() {
+   await fetch(statusUrl)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -54,19 +65,39 @@ const setUserTimerId = setInterval(() => {
       .then(data => {
           view_status = markup_status(data);
         // date_page.insertAdjacentHTML("beforeend", markup_status(data))
-        date_page.innerHTML = markup_mode(data) + markup_status(data);
+        // date_page.innerHTML = markup_mode(data) + markup_status(data);
+        date_page.innerHTML = view_mode + view_status;
       })
       .catch(error => {
         console.error('Fetch error:', error);
       });
   };
   
+async function percentage() {
+    await fetch(percentageUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        view_percentage = markup_percentage(data);
+        // date_page.innerHTML = markup_mode(data);
+        // date_page.innerHTML = markup_mode(data) + markup_status(data) + markup_percentage(data);
+        date_page.innerHTML = view_mode + view_status + view_percentage;
+        // date_page.insertAdjacentHTML("beforeend", markup_percentage(data))
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  };
 
 function markup_mode (data) {
   if (data["mode"]==="Line Mode") {
-  return (`<div class="mode_EP">
-     <p class="param_title net main">Живлення від мережі </p> </div>`)}
-  else { return (`<div class="mode_EP"><p class="param_title bat main">Живлення від батарей</p></div>`)};
+  return (`<div class="mode_EP main">
+     <p class="param_title net">Живлення від мережі </p> </div>`)}
+  else { return (`<div class="mode_EP main"><p class="param_title bat">Живлення від батарей</p></div>`)};
 };
 
 function markup_status (data) {
@@ -140,3 +171,11 @@ function markup_status (data) {
      </div>
      `)
 };
+
+function markup_percentage (data) {
+    return (`<div class="bat_percent">
+     <p class="param_title"> Поточний заряд батареї</p>
+     <p class="param_data"> ${data['charge_percent']}</p> 
+       </div>
+     `)
+    };
