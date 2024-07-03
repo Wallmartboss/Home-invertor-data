4dff2a25caae4bfd35dbbd6deffa6dbd231d2f8b
Yuri <wall.mart.boss@proton.me>
2024-07-03 17:52:46 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,79 +1,131 @@
+const date_page = document.querySelector('.date_page');
 
-// import { markupGallery } from './js/render-functions';
-
-// const form = document.querySelector('.search-form');
-// const input = form.querySelector('input');
-// let userData;
-// const gallery = document.querySelector('ul.gallery');
-// const loader = document.querySelector('.loader');
-
-
-
-// const results = getData();
-// results.then(({data}) => {
-//     const markup = reviewsMarkup(data);
-//     // reviewsList.innerHTML = markup;
-// })
-//   .catch((error) => {
-//           console.error('Виникла помилка: ', error);
-//       });
-  //  const errorMarkup = `<li class="error-review">
-  //  <p class="error-review-text">Not Found</p></li>`
-  //  return reviewsList.innerHTML = errorMarkup;
-// });
-// getData();
 const url = "http://192.168.1.117:9000/"; 
 const status_EP = "get_status";
 const mode_EP = "get_mode";
 const warnings_EP = "get_warnings"; 
-let totalUrl;
 
-totalUrl = `${url}${warnings_EP}`;
-
-fetch(totalUrl)
-  .then( response  => {
-    console.log(response);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Преобразование тела ответа в JSON
-  })
-  .then(data => {
-    // Обработка полученного JSON-объекта
-    console.log(data); // Вывод данных в консоль
-    const res = JSON.parse(data); // преобразование JSON в объект
-    // console.log(res);
-    // console.log(res['ac_output_active_power']);
-    
+const modeUrl = `${url}${mode_EP}`;
+const statusUrl = `${url}${status_EP}`;
+const warningsUrl = `${url}${warnings_EP}`;
 
 
-    
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
+mode();
+statusEP()
 
-  // fetch(url, { mode: 'no-cors' })
-  // .then(res => res.json())
-  // .then(res => {
-  //   this.setState({loaded: true, items: res.items})
-  // })
-  // .catch(err => {
-  //   console.log('OH SHIT:', err)
-  //   this.setState({loadError: true})
-  // })
+const setUserTimerId = setInterval(() => {
+  mode();
+  statusEP()
+}, 15000);
 
-  // const myImage = document.querySelector("img");
+  function mode() {
+    fetch(modeUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        date_page.innerHTML = markup_mode(data);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  };
 
-// const myRequest = new Request("http://192.168.1.117:9000/get_status");
+  function statusEP() {
+    fetch(statusUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        date_page.insertAdjacentHTML("beforeend", markup_status(data))
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  };
+  
 
-// fetch(myRequest, { mode: 'no-cors' })
-//   .then((response) => {
-//     console.log("response.type =", response.type); // response.type = 'basic'
-//     return response.blob();
-//   })
-//   .then((myBlob) => {
-//     const objectURL = URL.createObjectURL(myBlob);
-//     console.log(objectURL);
-//     // myImage.src = objectURL;
-//   });
+function markup_mode (data) {
+  if (data['mode']==="Line Mode") {
+  return (`<div class="mode_EP">
+     <p class="param_title net main">Живлення від мережі </p> </div>`)}
+  else { return (`<div class="mode_EP"><p class="param_title bat main">Живлення від батарей</p></div>`)};
+};
+
+function markup_status (data) {
+  return (`<div class="all_param">
+     <ul class="param_box">
+     <li class="param_item">
+     <p class="param_title">Вхідна напруга, В</p>
+     <p class="param_data">${data['ac_grid_voltage']}</p> 
+     </li>
+
+     <li class="param_item">
+     <p class="param_title">Вхідна частота, Hz</p>
+     <p class="param_data">${data['ac_grid_frequency']}</p> 
+     </li>
+ 
+     <li class="param_item">
+     <p class="param_title">Ємність батарей, %</p>
+     <p class="param_data">${data['battery_capacity']}</p> 
+     </li>
+
+     <li class="param_item">
+     <p class="param_title">Струм заряду батарей, А</p>
+     <p class="param_data">${data['battery_charging_current']}</p> 
+     </li>
+
+     <li class="param_item">
+     <p class="param_title">Струм розряду батарей, А</p>
+     <p class="param_data">${data['battery_discharge_current']}</p> 
+     </li>
+
+     <li class="param_item">
+     <p class="param_title">Вольтаж батарей, В</p>
+     <p class="param_data">${data['battery_voltage']}</p> 
+     </li>
+
+     <li class="param_item">
+     <p class="param_title">Температура інвертору, град.С</p>
+     <p class="param_data">${data['inverter_heat_sink_temperature']}</p> 
+     </li>
+     </ul>
+     
+
+     <ul class="param_box">
+     <li class="param_item">
+     <p class="param_title">Вихідна напруга, В </p>
+     <p class="param_data">${data['ac_output_voltage']} </p> 
+     </li>
+
+    <li class="param_item">
+     <p class="param_title">Вихідна частота, Hz</p>
+     <p class="param_data">${data['ac_output_frequency']} </p> 
+     </li>
+ 
+    <li class="param_item">
+     <p class="param_title"> Навантаження інвертору, %</p>
+     <p class="param_data"> ${data['ac_output_load_percent']} </p> 
+     </li>
+    <li class="param_item">
+     <p class="param_title">Споживана від інвертору потужність, Вт</p>
+     <p class="param_data">${data['ac_output_active_power']}</p> 
+     </li>
+    <li class="param_item">
+     <p class="param_title">Споживана від інвертору потужність, ВА</p>
+     <p class="param_data">${data['ac_output_apparent_power']}</p> 
+     </li>
+     <li class="param_item">
+     <p class="param_title"> Чи заряджаються батареї?</p>
+     <p class="param_data"> ${data['is_ac_charging_on']}</p> 
+     </li>
+     </ul>
+     </div>
+     `)
+};
